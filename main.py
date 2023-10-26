c4b17feddb7c0931f80e817a6b9cd82ad217f585
Roberto Catalán <rocatalan@gmail.com>
2023-10-26 00:07:03 +0000

    fix: soluciona nombre de secret

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 def get_db_connection():
     conn = psycopg2.connect(host=os.environ['DB_HOST'],
-                            database=os.environ['DB_PASSWORD'],
+                            database=os.environ['DB_DATABASE'],
                             user=os.environ['DB_USERNAME'],
                             password=os.environ['DB_PASSWORD'])
     return conn
